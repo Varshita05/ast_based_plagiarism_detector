ca883a4e9a5250c4892911cbca77c3c412c2dec2
Varshita05 <varshitaposani.05@gmail.com>
2026-04-05 18:21:02 +0530

    small change: added vercel app url to allowed origins in CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title = "AST Code Plagiarism Detector")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://ast-based-plagiarism-detector.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
